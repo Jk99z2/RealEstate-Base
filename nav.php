31ec2e04507e1a794049e32b2448cbf63389f50a
Enrique Ochoa <enriquej.ochoap@outlook.es>
2022-12-10 13:04:23 -0600

    Base template - Real Estate Project
    Real Estate start project

---
+++ nav.php
@@ -0,0 +1,41 @@
+<div class="top">
+    	<div class="container">
+    		<div class="row d-flex align-items-center">
+    			<div class="col">
+    				<p class="social d-flex">
+    					<a href="#"><span class="icon-facebook"></span></a>
+    					<a href="#"><span class="icon-twitter"></span></a>
+    					<a href="#"><span class="icon-google"></span></a>
+							<a href="#"><span class="icon-pinterest"></span></a>
+    				</p>
+    			</div>
+    			<div class="col d-flex justify-content-end">
+    				<p class="num"><span class="icon-phone"></span> + 1700 12345 6789</p>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index.html">Royal<span>estate</span></a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="property.html" class="nav-link">Property</a></li>
+	          <li class="nav-item"><a href="agents.html" class="nav-link">Agents</a></li>
+	          <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+	          <li class="nav-item cta"><a href="contact.html" class="nav-link ml-lg-2"><span class="icon-user"></span> Sign-In</a></li>
+	          <li class="nav-item cta cta-colored"><a href="contact.html" class="nav-link"><span class="icon-pencil"></span> Sign-Up</a></li>
+
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->

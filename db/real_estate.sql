5413c162e72b9bfd94ccffddc935f5f795fca26e
Enrique Ochoa <enriquej.ochoap@outlook.es>
2022-12-17 13:48:39 -0600

    Update db backup

--- db/real_estate.sql
+++ db/real_estate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2022 at 07:26 PM
+-- Generation Time: Dec 17, 2022 at 08:47 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 

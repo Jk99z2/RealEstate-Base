d3a053e52bc67163151d99db28c5da22e9e04064
Enrique Ochoa <enriquej.ochoap@outlook.es>
2022-12-17 13:49:25 -0600

    Creación dashboard base
    Se crea un dashboard base sencillo que redirigirá a las secciones deseadas (check href)

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if(!isset($_SESSION['admin'])){
+    header('location: ../index.php');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +14,63 @@
     <?php include '../links.php'; ?>
 </head>
 <body>
-    <?php include '../nav.php'; ?>
+    <?php include '../nav.php'; 
+    ?>
+
+    <section class="container py-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="font-weight-bold text-center py-1">Dashboard</h2>
+                    </div>
+                    <div class="card-body"></div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-img-top pt-4">
+                                    <a href="adm_properties">    
+                                        <center><img src="../images/icons/home.png" class="card-img-top" alt="" style="width: 5rem; height: 5rem;"></center><br>
+                                    </a>
+                                </div>
+                                <div class="card-header">
+                                    <a href="adm_properties">
+                                        <h3 class="text-center">Gestionar propiedades</h3>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card">
+                            <div class="card-img-top pt-4">
+                                    <a href="adm_users">
+                                        <center><img src="../images/icons/user.png" class="card-img-top" alt="" style="width: 5rem; height: 5rem;"></center><br>
+                                    </a>
+                                </div>
+                                <div class="card-header">
+                                    <a href="adm_users">
+                                        <h3 class="text-center">Gestionar agentes</h3>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card">
+                            <div class="card-img-top pt-4">
+                                <a href="settings">
+                                    <center><img src="../images/icons/enterprise.png" class="card-img-top" alt="" style="width: 5rem; height: 5rem;"></center><br>
+                                </a>
+                                </div>
+                                <div class="card-header">
+                                    <a href="settings">
+                                        <h3 class="text-center">Ajustes</h3>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+    </section>
 </body>
     <?php include '../footer.php'; ?>
 	

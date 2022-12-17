c0d3625aab32a2f4ce7513e0a052df17328a946f
Enrique Ochoa <enriquej.ochoap@outlook.es>
2022-12-17 12:27:37 -0600

    Creación base dashboard
    Se crea la base para el dashboard del administrador.

---
+++ admin/dashboard.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Admin</title>
+    <?php include '../links.php'; ?>
+</head>
+<body>
+    <?php include '../nav.php'; ?>
+</body>
+    <?php include '../footer.php'; ?>
+	
+    <!-- loader -->
+      <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+  
+      <?php include '../scripts.php'; ?>
+</html>
